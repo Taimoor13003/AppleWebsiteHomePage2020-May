c4589714fd9ed86c2221fb7ba0660b990a50c20a
Taimoor13003 <taimoorghori@rocketmail.com>
2020-08-08 19:09:03 +0500

    Updates

---
+++ precache-manifest.369dba81d2474edfe6aa9f7e357d3e4a.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1718a8d67805b5ab63894c82dae84ba5",
+    "url": "/AppleWebsiteHomePage2020-May/index.html"
+  },
+  {
+    "revision": "03896470ff922d0b819c",
+    "url": "/AppleWebsiteHomePage2020-May/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "03f8b119e3b813f03a3e",
+    "url": "/AppleWebsiteHomePage2020-May/static/css/main.dfbbd7d0.chunk.css"
+  },
+  {
+    "revision": "03896470ff922d0b819c",
+    "url": "/AppleWebsiteHomePage2020-May/static/js/2.ea114ab3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/AppleWebsiteHomePage2020-May/static/js/2.ea114ab3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03f8b119e3b813f03a3e",
+    "url": "/AppleWebsiteHomePage2020-May/static/js/main.ec84422b.chunk.js"
+  },
+  {
+    "revision": "f1e8cb3ba760c45ab6d9",
+    "url": "/AppleWebsiteHomePage2020-May/static/js/runtime-main.9d9822a3.js"
+  },
+  {
+    "revision": "f044563831c671d33a4cf8c1d03b4a80",
+    "url": "/AppleWebsiteHomePage2020-May/static/media/1.f0445638.jpg"
+  },
+  {
+    "revision": "05c9fee81df484252e72e29ea2d358cb",
+    "url": "/AppleWebsiteHomePage2020-May/static/media/14.05c9fee8.jpg"
+  },
+  {
+    "revision": "7e874c32a9bae37316fc96e5a7c30f7a",
+    "url": "/AppleWebsiteHomePage2020-May/static/media/2.7e874c32.jpg"
+  },
+  {
+    "revision": "2967804604ee3c0d994a33e2be1c297d",
+    "url": "/AppleWebsiteHomePage2020-May/static/media/22.29678046.jpg"
+  },
+  {
+    "revision": "3c47bccab318c133f5b77f3360227c6d",
+    "url": "/AppleWebsiteHomePage2020-May/static/media/24.3c47bcca.jpg"
+  },
+  {
+    "revision": "a9b2e56d60b943381965a0b0cb737bc3",
+    "url": "/AppleWebsiteHomePage2020-May/static/media/3.a9b2e56d.jpg"
+  },
+  {
+    "revision": "9c6d6d80ccd9bff1be9408c47c27192a",
+    "url": "/AppleWebsiteHomePage2020-May/static/media/32.9c6d6d80.jpg"
+  },
+  {
+    "revision": "7403a7f75a0cea87a5c4d93030b07be8",
+    "url": "/AppleWebsiteHomePage2020-May/static/media/34.7403a7f7.jpg"
+  },
+  {
+    "revision": "643fddec5cc11856ddba20d6811203a2",
+    "url": "/AppleWebsiteHomePage2020-May/static/media/4.643fddec.jpg"
+  },
+  {
+    "revision": "84e96ee6f42a6108078acd2732907519",
+    "url": "/AppleWebsiteHomePage2020-May/static/media/42.84e96ee6.jpg"
+  },
+  {
+    "revision": "cef4263133aff12f48acf2e30e1773be",
+    "url": "/AppleWebsiteHomePage2020-May/static/media/43.cef42631.jpg"
+  },
+  {
+    "revision": "220b454e3b0beaa5d282ef17fbdeaac2",
+    "url": "/AppleWebsiteHomePage2020-May/static/media/44.220b454e.jpg"
+  },
+  {
+    "revision": "7b98a5cbe2ac7c7d3c385c16b4feb47b",
+    "url": "/AppleWebsiteHomePage2020-May/static/media/5.7b98a5cb.jpg"
+  },
+  {
+    "revision": "d8fa3e45767f4b9351bd48269d566965",
+    "url": "/AppleWebsiteHomePage2020-May/static/media/52.d8fa3e45.jpg"
+  },
+  {
+    "revision": "744e7b94b0070924a2f577e1e98094f5",
+    "url": "/AppleWebsiteHomePage2020-May/static/media/53.744e7b94.jpg"
+  },
+  {
+    "revision": "05da1469cfde9088791b934f0a6c3d28",
+    "url": "/AppleWebsiteHomePage2020-May/static/media/54.05da1469.jpg"
+  },
+  {
+    "revision": "7f3606b6defa3bde176be54fe207f34a",
+    "url": "/AppleWebsiteHomePage2020-May/static/media/62.7f3606b6.jpg"
+  },
+  {
+    "revision": "841887dc968f1592160295421aa9bd4a",
+    "url": "/AppleWebsiteHomePage2020-May/static/media/64.841887dc.jpg"
+  },
+  {
+    "revision": "b53f151f954ad9003aa0c977837c3b94",
+    "url": "/AppleWebsiteHomePage2020-May/static/media/7.b53f151f.jpg"
+  },
+  {
+    "revision": "c886bcca4068baeb9f5a774b66569da1",
+    "url": "/AppleWebsiteHomePage2020-May/static/media/8.c886bcca.jpg"
+  },
+  {
+    "revision": "a3aa863098cadde7811d060c19a577a5",
+    "url": "/AppleWebsiteHomePage2020-May/static/media/usLogo.a3aa8630.png"
+  }
+]);
